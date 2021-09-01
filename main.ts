e67cd3e6b35def847184e6b3138349173e8293c7
badch <50537232+badch@users.noreply.github.com>
2021-09-01 22:44:42 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+    basic.pause(100)
+    basic.showNumber(1)
+    basic.pause(100)
+    basic.showNumber(2)
+    basic.pause(100)
+    basic.showNumber(3)
+    basic.pause(100)
+    basic.showNumber(4)
+    basic.pause(100)
+    basic.showNumber(4)
+})
